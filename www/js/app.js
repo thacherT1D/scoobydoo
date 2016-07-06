7386b852a697e375aaabd07efc3b58199ec71603
THACHER <thachert1d@gmail.com>
2016-07-05 17:42:50 -0700

    app.js updates

--- www/js/app.js
+++ www/js/app.js
@@ -1,74 +1,67 @@
-// Ionic Starter App
+(function() {
+  'use strict';
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('scoobydoo', ['ionic', 'ngCordova', 'angularMoment'])
+  angular
+    .module('scoobydoo', ['ionic', 'ngCordova', 'angularMoment'])
+    .config(routeHandler)
+    .run(runBlock);
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
+    routeHandler.$inject = ['$stateProvider', '$urlRouterProvider'];
+    runBlock.$inject = ['$ionicPlatform'];
 
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+    function routeHandler($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/tab/home')
+      $stateProvider
+      // setup an abstract state for the tabs directive
+        .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
 
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.home', {
-    url: '/home',
-    views: {
-      'tab-home': {
-        templateUrl: 'templates/tab-home.html',
-        controller: 'HomeCtrl as ctrl'
-      }
-    }
-  })
-
-  .state('tab.data', {
-      url: '/data',
-      views: {
-        'tab-data': {
-          templateUrl: 'templates/tab-data.html',
-          controller: 'DataCtrl as ctrl'
+      // Each tab has its own nav history stack:
+      .state('tab.home', {
+        url: '/home',
+        views: {
+          'tab-home': {
+            templateUrl: 'templates/tab-home.html',
+            controller: 'HomeCtrl as ctrl'
+          }
         }
-      }
-    })
+      })
 
-  .state('tab.about', {
-    url: '/about',
-    views: {
-      'tab-about': {
-        templateUrl: 'templates/tab-about.html',
-        controller: 'AboutCtrl as ctrl'
-      }
+      .state('tab.data', {
+          url: '/data',
+          views: {
+            'tab-data': {
+              templateUrl: 'templates/tab-data.html',
+              controller: 'DataCtrl as ctrl'
+            }
+          }
+        })
+
+      .state('tab.about', {
+        url: '/about',
+        views: {
+          'tab-about': {
+            templateUrl: 'templates/tab-about.html',
+            controller: 'AboutCtrl as ctrl'
+          }
+        }
+      })
     }
-  });
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+    function runBlock($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+          cordova.plugins.Keyboard.disableScroll(true);
+        }
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleDefault();
+        }
+      });
+    }
 
-});
+})();
